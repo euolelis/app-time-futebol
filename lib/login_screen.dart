f82f15c5580425ea9138376d64fdfae18073cc89
Ynaia Yleran Lelis <euolelis@iMac-de-Ynaia.local>
2025-10-08 18:28:17 -0300

    Atualização

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF5B1A18), // Cor de fundo da tela
+      backgroundColor: const Color(0xFF5B1A18), // Cor de fundo da tela
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'MOREIRA\'S SPORT',
               style: TextStyle(
                 fontSize: 32,
@@ -18,39 +20,39 @@ class LoginScreen extends StatelessWidget {
                 color: Color(0xFFFFD700),
               ),
             ),
-            SizedBox(height: 40),
-            TextField(
+            const SizedBox(height: 40),
+            const TextField(
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Colors.white,
                 hintText: 'Email',
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
                 ),
               ),
             ),
-            SizedBox(height: 20),
-            TextField(
+            const SizedBox(height: 20),
+            const TextField(
               obscureText: true,
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Colors.white,
                 hintText: 'Senha',
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
                 ),
               ),
             ),
-            SizedBox(height: 30),
+            const SizedBox(height: 30),
             ElevatedButton(
               onPressed: () {
                 // Navegar para a tela inicial
                 Navigator.pushReplacementNamed(context, '/home');
               },
               style: ElevatedButton.styleFrom(
-                primary: Color(0xFFFFD700),
+                backgroundColor: const Color(0xFFFFD700),
               ),
-              child: Text(
+              child: const Text(
                 'Entrar',
                 style: TextStyle(color: Color(0xFF5B1A18)),
               ),
